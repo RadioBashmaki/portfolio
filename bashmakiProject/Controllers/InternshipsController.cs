c4c2dfc3e2041072e72214f8c9c4b10316cbefc3
HTFDT <almetov_tag@mail.ru>
2023-06-14 14:06:20 +0500

    added edit internship page

--- bashmakiProject/Controllers/InternshipsController.cs
+++ bashmakiProject/Controllers/InternshipsController.cs
@@ -66,7 +66,117 @@ public class InternshipsController : Controller
     public IActionResult AddFile([Bind("FilesDescriptions")] CreateInternshipRequest createRequest)
     {
         ViewData["index"] = createRequest.FilesDescriptions?.Length ?? 0;
-        return PartialView("_AddFilePartial");
+        return PartialView("_ModalPartial");
+    }
+
+    [Authorize(Policy = "OnlyForRepresentatives")]
+    [HttpGet("{id}/edit")]
+    public async Task<IActionResult> EditInternship([FromRoute] string id)
+    {
+        if (!MongoDB.Bson.ObjectId.TryParse(id, out _))
+            return NotFound();
+        var user = await GetCurrentUser();
+        var internshipsCollection = _repository.GetCollection<Internship>();
+        var filter = Builders<Internship>.Filter
+            .Eq(i => i.Id, id);
+        var currentInternship = await internshipsCollection.Find(filter).FirstOrDefaultAsync();
+        if (currentInternship == null)
+            return NotFound();
+        if (currentInternship.UserId != user.Id)
+            return Forbid();
+        ViewData["internshipId"] = currentInternship.Id;
+        ViewData["internshipTitle"] = currentInternship.Title;
+
+        var files = new List<EditProjectFileDescription>();
+        if (currentInternship.Files != null)
+            files.AddRange(currentInternship.Files
+                .Select(desc => new EditProjectFileDescription
+                {
+                    DatabaseId = desc.Id, Name = desc.Name, Description = desc.Description, Extension = desc.Extension
+                }));
+        var edit = new EditInternshipRequest
+        {
+            Title = currentInternship.Title,
+            Description = currentInternship.Description,
+            FilesDescriptions = files.ToArray(),
+            Topics = Enum.GetValues<Topic>().ToDictionary(x => x, y => currentInternship.Topics.Contains(y))
+        };
+        
+        return View(edit);
+    }
+
+    [Authorize(Policy = "OnlyForRepresentatives")]
+    [HttpPost("{id}/edit")]
+    public async Task<IActionResult> EditInternship([FromRoute] string id, [FromForm] EditInternshipRequest editRequest)
+    {
+        if (!MongoDB.Bson.ObjectId.TryParse(id, out _))
+            return NotFound();
+        var user = await GetCurrentUser();
+        var internshipsCollection = _repository.GetCollection<Internship>();
+        var filter = Builders<Internship>.Filter
+            .Eq(i => i.Id, id);
+        var currentInternship = await internshipsCollection.Find(filter).FirstOrDefaultAsync();
+        if (currentInternship == null)
+            return NotFound();
+        if (currentInternship.UserId != user.Id)
+            return Forbid();
+        if (!ModelState.IsValid)
+        {
+            var errorsWithFiles = ModelState.Count(item =>
+                item.Key.EndsWith("File") && item.Value.ValidationState == ModelValidationState.Invalid);
+            if (errorsWithFiles != ModelState.ErrorCount)
+                return View(editRequest);
+        }
+        
+        var editedFiles = editRequest.FilesDescriptions?.Where(x => x.DatabaseId != null).ToDictionary(x => x.DatabaseId!);
+
+        currentInternship.Title = editRequest.Title;
+        currentInternship.Description = editRequest.Description;
+        currentInternship.Topics = editRequest.Topics.Keys.Where(key => editRequest.Topics[key]).ToArray();
+        currentInternship.IsActive = editRequest.IsActive;
+        if (editRequest.FilesDescriptions == null || editRequest.FilesDescriptions.Length == 0 ||
+            editedFiles == null || editedFiles.Count == 0)
+            currentInternship.Files = null;
+        else if (currentInternship.Files != null)
+        {
+            var oldFiles = currentInternship.Files.ToDictionary(x => x.Id);
+            var filesLeft = editedFiles.Keys.Select(key =>
+            {
+                var newFile = new FileDescriptionDatabase
+                {
+                    Name = editedFiles[key].Name,
+                    Description = editedFiles[key].Description
+                };
+                if (editedFiles[key].File != null)
+                {
+                    newFile.File = FormFileToByteArray(editedFiles[key].File!).Result;
+                    newFile.Extension = Path.GetExtension(editedFiles[key].File!.FileName);
+                    newFile.ContentType = editedFiles[key].File!.ContentType;
+                }
+                else
+                {
+                    newFile.File = oldFiles[key].File;
+                    newFile.Extension = oldFiles[key].Extension;
+                    newFile.ContentType = oldFiles[key].ContentType;
+                }
+                return newFile;
+            });
+            currentInternship.Files = filesLeft.ToArray();
+        }
+        
+        var filesList = currentInternship.Files?.ToList() ?? new List<FileDescriptionDatabase>();
+        foreach (var newFile in editRequest.FilesDescriptions?.Where(f => f.DatabaseId == null) ?? Array.Empty<EditProjectFileDescription>())
+            filesList.Add(new FileDescriptionDatabase
+            {
+                Name = newFile.Name,
+                Description = newFile.Description,
+                File = await FormFileToByteArray(newFile.File!),
+                ContentType = newFile.File!.ContentType,
+                Extension = Path.GetExtension(newFile.File!.FileName)
+            });
+        currentInternship.Files = filesList.ToArray();
+        await internshipsCollection.ReplaceOneAsync(filter, currentInternship);
+        return View(editRequest);
     }
 
     [NonAction]
---
+++ bashmakiProject/Models/EditInternshipRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bashmakiProject.Models;
+
+public class EditInternshipRequest
+{
+    [Required(ErrorMessage = "Это обязательное поле")]
+    public string Title { get; set; }
+    public Dictionary<Topic, bool> Topics { get; set; }
+    [Required(ErrorMessage = "Это обязательное поле")]
+    public Experience Experience { get; set; }
+    [Required(ErrorMessage = "Это обязательное поле")]
+    [StringLength(5000)]
+    public string Description { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public EditProjectFileDescription[]? FilesDescriptions { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var errors = new List<ValidationResult>();
+        if (Topics.Values.All(x => x == false))
+            errors.Add(new ValidationResult("Выберите хотя бы одну тематику", new List<string> { "Topics" }));
+        return errors;
+    }
+}
